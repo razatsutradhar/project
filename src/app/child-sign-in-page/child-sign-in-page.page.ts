d177788ae1d1aea1c377843f86837f77c300ea17
razatsutradhar <razat74@gmail.com>
2020-11-21 11:04:54 -0500

    fixed double import issue in child sign in page

--- src/app/child-sign-in-page/child-sign-in-page.page.ts
+++ src/app/child-sign-in-page/child-sign-in-page.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Component} from '@angular/core';
 import { Router } from '@angular/router';
 
 const childSignInBtn = document.querySelector('#child-button-confirm');
